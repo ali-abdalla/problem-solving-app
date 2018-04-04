721098c2ddce3d6509d038287badc4584df08517
Ali Abdallah <aliabdallah93@gmail.com>
2018-04-04 10:57:09 +0200

    add two way binding for input and output, add run button

--- problem-solving-app/src/app/app.component.ts
+++ problem-solving-app/src/app/app.component.ts
@@ -7,5 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   editorOptions = {theme: 'vs-dark', language: 'javascript'};
-  code: string= 'function x() {\nconsole.log("Hello world!");\n}';
+  code: string= `function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+    
+    // Write the second line of output that prints the contents of 'inputString' here.
+  } 
+
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  _input = "";
+  process.stdin.on("data", function (input) {
+      _input += input;
+  });
+
+  process.stdin.on("end", function () {
+    processData(_input);
+  });
+  `;
+
+  input = "";
+  output = "";
+
+  run() {
+    console.log('code: ', this.code);
+    console.log('input: ', this.input);
+    console.log('output: ', this.output);
+  }
 }
