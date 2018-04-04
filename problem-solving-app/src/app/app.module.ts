413d431113b80c0c19544aa5c0898877e1818888
Ali Abdallah <aliabdallah93@gmail.com>
2018-04-04 10:22:49 +0200

    add ngx-monaco-editor and run it

--- problem-solving-app/src/app/app.module.ts
+++ problem-solving-app/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { MonacoEditorModule } from 'ngx-monaco-editor';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MonacoEditorModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
