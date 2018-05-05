62faab454eaf89e25032e40be272cb798ffe8779
Ali Abdallah <aliabdallah93@gmail.com>
2018-05-05 14:28:27 +0200

    fix tabs in default source codes

--- client/src/app/source-code/source-codes.ts
+++ client/src/app/source-code/source-codes.ts
@@ -6,27 +6,27 @@ var input_stdin_array = "";
 var input_currentline = 0;
 
 process.stdin.on('data', function (data) {
-  input_stdin += data;
+    input_stdin += data;
 });
 
 process.stdin.on('end', function () {
-  input_stdin_array = input_stdin.split("\\n");
-  main();
+    input_stdin_array = input_stdin.split("\\n");
+    main();
 });
 
 function readLine() {
-  return input_stdin_array[input_currentline++];
+    return input_stdin_array[input_currentline++];
 }
 
 /////////////// ignore above this line ////////////////////
 
 function main() {
-  var N = parseInt(readLine());
-  if (N % 2) {
-    console.log("Weird");
-  } else {
-    console.log("Not Weird");
-  }
+    var N = parseInt(readLine());
+    if (N % 2) {
+        console.log("Weird");
+    } else {
+        console.log("Not Weird");
+    }
 }
 `;
 
